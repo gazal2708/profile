58cb9c22336e57c9e4f0bc0ee4bb15c5d15e305a
gazal2708 <gazaljain7523@gmail.com>
2020-05-12 17:43:01 +0530

    Updates

---
+++ precache-manifest.e472af71c6763a5d6d378ce1fd14c2d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf5c9fa9206835bf939c185ed71e7849",
+    "url": "/index.html"
+  },
+  {
+    "revision": "101650611bfb4555ee22",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "7740d61336bc32d2b607",
+    "url": "/static/js/2.9d1d5374.chunk.js"
+  },
+  {
+    "revision": "101650611bfb4555ee22",
+    "url": "/static/js/main.f16db069.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c8f88ebd8b016196141c08cf9d64e7f0",
+    "url": "/static/media/profile.c8f88ebd.jpg"
+  }
+]);
